218de1c93bcd1acc3011cd8119a411a56d181390
umidjons <almatov.us@gmail.com>
2019-03-30 15:15:23 +0500

    Cart details page is implemented

--- src/shop/ShopConnector.js
+++ src/shop/ShopConnector.js
@@ -6,6 +6,7 @@ import { loadData } from '../data/ActionCreators';
 import { DataTypes } from '../data/Types';
 import { Shop } from './Shop';
 import { addToCart, updateCartQuantity, removeFromCart, clearCart } from '../data/CartActionCreators';
+import { CartDetails } from './CartDetails';
 
 const mapStateToProps = dataStore => ({...dataStore});
 const mapDispatchToProps = {loadData, addToCart, updateCartQuantity, removeFromCart, clearCart};
@@ -20,6 +21,7 @@ export const ShopConnector = connect(mapStateToProps, mapDispatchToProps)(
           <Route path="/shop/products/:category?"
             render={ routeProps => 
               <Shop { ...this.props } { ...routeProps } products={ filterProducts(this.props.products, routeProps.match.params.category) } /> } />
+          <Route path="/shop/cart" render={ routeProps => <CartDetails { ...this.props } { ...routeProps } /> } />
           <Redirect to="/shop/products" />
         </Switch>
     }
