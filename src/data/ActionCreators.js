f66920a6fd01d94992c6180f8d527c642dc718a7
umidjons <almatov.us@gmail.com>
2019-03-31 13:50:50 +0500

    Implemented pagination on the products page

--- src/data/ActionCreators.js
+++ src/data/ActionCreators.js
@@ -3,10 +3,21 @@ import { RestDataSource } from './RestDataSource';
 
 const dataSource = new RestDataSource();
 
-export const loadData = dataType => ({
+export const loadData = (dataType, params) => ({
     type: ActionTypes.DATA_LOAD,
     payload: {
         dataType,
-        data: dataSource.GetData(dataType).then(response => ({dataType, data: response.data}))
+        data: dataSource.GetData(dataType, params)
+            .then(response => ({
+                    dataType,
+                    data: response.data,
+                    total: Number(response.headers['x-total-count']),
+                    params
+                })
+            )
     }
 });
+
+export const setPageSize = (newSize) => ({type: ActionTypes.DATA_SET_PAGESIZE, payload: newSize});
+
+export const setSortProperty = (newProperty) => ({type: ActionTypes.DATA_SET_SORT_PROPERTY, payload: newProperty});
